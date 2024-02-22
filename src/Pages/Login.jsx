f095543b18b99b4f3ae113ac9493eeb7afb7c06e
Raúl Alvarez <raul509@gmail.com>
2024-02-22 01:07:58 -0400

    vol2

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { dataDecrypt } from "../components/functionEncryp";
 import { validador } from "../constant/secretkey";
 import '../styles/login.css'
+import { NavLink } from "react-router-dom";
 
 function Login() {
 
@@ -48,11 +49,11 @@ function Login() {
                 <form className='flex flex-col m-3 gap-4 items-center bg-transparent'>
                     <label className='flex items-center border border-gray-800 rounded-lg gap-2 p-1 shadow-lg bg-white'>
                         <i className='bx bx-envelope m-2' ></i>
-                        <input type="Email" placeholder="Ingresa tu Email" className='p-1 rounded-md' value={email} onChange={(event) => setEmail(event.target.value)} required/>
+                        <input type="Email" placeholder="Ingresa tu Email" className='p-1 rounded-md' value={email} onChange={(event) => setEmail(event.target.value)} required />
                     </label>
                     <label className='flex items-center border border-gray-800 rounded-lg gap-2 p-1 shadow-lg bg-white '>
                         <i className='bx bx-lock-alt m-2' ></i>
-                        <input type="password" placeholder="Contraseña" className='p-1 rounded-md ' value={password} onChange={(event) => setPassword(event.target.value)} required/>
+                        <input type="password" placeholder="Contraseña" className='p-1 rounded-md ' value={password} onChange={(event) => setPassword(event.target.value)} required />
                     </label>
                     <input type="submit" value="Iniciar Sesión" className='bg-cyan-600 hover:bg-cyan-400 text-white font-bold py-2 px-6 rounded-full m-8' onClick={hanledVerified} />
                 </form>
@@ -60,6 +61,9 @@ function Login() {
                     <i className='bx bxl-google bg-white text-red-600 hover:bg-gray-400 text-gray-800 font-semibold py-3 px-3 border border-black rounded-full shadow m-2'></i>
                     <i className='bx bxl-github bg-white text-black hover:bg-gray-400 hover:text-black font-semibold py-3 px-3 border border-black rounded-full shadow m-2' ></i>
                     <i className='bx bxl-linkedin bg-white text-blue-700 hover:bg-gray-400 text-gray-800 font-semibold py-3 px-3 border border-black rounded-full shadow m-2' ></i>
+                </div>
+                <div>
+                    <NavLink to="/" className="text-white hover:text-gray-500">Registro</NavLink>
                 </div>
             </div>
         </>
--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -6,24 +6,23 @@ export default function Chart() {
     if (usuario) {
         arrA.push(usuario)
     }
-    console.log(arrA[0])
-    
+ 
     const renderListado = () =>{
-        const hanledEliminate = () =>{
-           
-           
-          
-
-        }
       return arrA[0].map((value, index) =>{
           return(
             <div key={index} className="chart-t flex " >
               <h2>Nombre:{value.nombre} </h2>
               <h2>Correo:{value.email} </h2>
               <h2>rol:{value.rol}</h2>
-            <button onClick={hanledEliminate} className="bg-red-600  text-white py-1 px-2 rounded-full  justify-center "><i className='bx bx-x-circle'></i></button>
-            </div>
-          )
+            <button className="bg-red-600  text-white py-1 px-2 rounded-full  justify-center " onClick={()=>{
+                if(data[index].rol !== "admin"){
+                    alert("paso")
+                    data.splice(index,1)
+                    localStorage.setItem("usuarios", JSON.stringify(data))
+                }
+            }}><i className='bx bx-x-circle'></i></button>
+            </div>            
+       )
        })
     }
     return (
