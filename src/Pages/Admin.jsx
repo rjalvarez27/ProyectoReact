230f0bab934e740f9f86983a79405911bb135bbf
Raúl Alvarez <raul509@gmail.com>
2024-02-22 00:31:47 -0400

    vol1

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -5,32 +5,17 @@ import CloseSesion from '../components/CloseSession.jsx'
 import Chart from "../components/Chart.jsx";
 import { useState, useEffect } from "react";
 import "../styles/admin.css"
-let arrA = []
-
 
 function Admin() {
 
     const [token, setToken] = useState(localStorage.getItem('token'));
     const [toggle, setToggle] = useState("light");
-    const [info,setInfo] = useState(arrA)
+    
 
     const hanledMode = () => {
         setToggle(prevTheme => prevTheme === "light" ? "dark" : "light")
     }
-    // datos de local que no son admin
-    const database = JSON.parse(localStorage.getItem("usuarios"))
-    const user = database.filter((e) => e.rol != "admin")
-    if (user) {
-        arrA.push(user)
-    }
-    console.log(info)
-
-    const hanledEliminate = () => {
-       
-    }
-
-  
-
+    
     useEffect(() => {
         const data = JSON.parse(localStorage.getItem('usuarios'))
         const usuario = data.find((e) => e.rol == "admin")
@@ -38,7 +23,6 @@ function Admin() {
             console.log("paso a admin")
         } else {
             setToken("")
-            setInfo("")
             window.location.href = '/login'
         }
     }, [token])
@@ -47,7 +31,6 @@ function Admin() {
 
     return (
         <>
-
             <div className={toggle}>
                 <Navbar />
                 <div className="container-admin">
@@ -60,9 +43,7 @@ function Admin() {
                     </div>
                     <div className='container-admin2 dark:bg-black'>
                         <div className="flex flex-row">
-                            < Chart nombre={info[0].nombre} correo={info[0].email} rol={info[0].rol} />
-                            <button onClick={hanledEliminate} className="bg-red-600 text-white font-bold  rounded-lg py-3 px-3 ml-2"><i className='bx bx-x-circle'></i></button>
-                           
+                            < Chart/>
                         </div>
                         <div>
                             <CloseSesion />
--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,18 +1,36 @@
-
 import "../styles/chart.css"
-
-
-export default function Chart (props){
- 
+let arrA = []
+export default function Chart() {
+    const data = JSON.parse(localStorage.getItem("usuarios"))
+    const usuario = data.filter((e) => e.rol != "admin")
+    if (usuario) {
+        arrA.push(usuario)
+    }
+    console.log(arrA[0])
     
+    const renderListado = () =>{
+        const hanledEliminate = () =>{
+           
+           
+          
 
- return(
-    <>
-    <div className="chart-t">
-    <h2>Nombre: {props.nombre}</h2>
-    <h2>Correo: {props.correo}</h2>
-    <h2>rol:{props.rol}</h2>
-    </div>
-    </>
- )
+        }
+      return arrA[0].map((value, index) =>{
+          return(
+            <div key={index} className="chart-t flex " >
+              <h2>Nombre:{value.nombre} </h2>
+              <h2>Correo:{value.email} </h2>
+              <h2>rol:{value.rol}</h2>
+            <button onClick={hanledEliminate} className="bg-red-600  text-white py-1 px-2 rounded-full  justify-center "><i className='bx bx-x-circle'></i></button>
+            </div>
+          )
+       })
+    }
+    return (
+       <> 
+       <div >
+        {renderListado()}
+       </div>
+       </>  
+    )
 }
